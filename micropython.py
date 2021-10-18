0e8329aa255d02d8be544978d00657da4e6687c1
Talescruzs <tcs2017@hotmail.com>
2021-10-18 12:09:53 -0300

    add configs padrão no objeto e contador de voltas

--- micropython.py
+++ micropython.py
@@ -2,14 +2,21 @@ import time
 import machine
 import framebuf
 
-scl = machine.Pin('X9')
-sda = machine.Pin('X10')
-i2c = machine.I2C(scl=scl, sda=sda)
-fbuf = framebuf.FrameBuffer(bytearray(64 * 32 // 8), 64, 32, framebuf.MONO_HLSB)
-
 class pomodoro(object):
     def __init__(self):
+        self.patterns()
         self.estudar()
+    def patterns(self):
+        global scl
+        global sda
+        global i2c
+        global fbuf
+        global volta
+        volta = 1
+        scl = machine.Pin('X9')
+        sda = machine.Pin('X10')
+        i2c = machine.I2C(scl=scl, sda=sda)
+        fbuf = framebuf.FrameBuffer(bytearray(64 * 32 // 8), 64, 32, framebuf.MONO_HLSB, 64)
     def estudar(self):
         self.tela_estudo()
         time.sleep(1500)
@@ -17,16 +24,20 @@ class pomodoro(object):
     def descansar(self):
         self.tela_descanso()
         time.sleep(300)
+        global volta
+        volta+=1
         self.estudar()
     def tela_estudo(self):
         fbuf.fill(0)
-        fbuf.text('WORK', 16, 7, 1)
-        fbuf.text('TIME', 16, 17, 1)
+        fbuf.text('WORK', 16, 3, 1)
+        fbuf.text('TIME', 16, 12, 1)
+        fbuf.text('VOLTA:%d'%volta, 4, 22, 1)
         return i2c.writeto(8, fbuf)
     def tela_descanso(self):
         fbuf.fill(1)
-        fbuf.text('RELAX', 12, 7, 0)
-        fbuf.text('TIME', 16, 17, 0)
+        fbuf.text('RELAX', 12, 3, 0)
+        fbuf.text('TIME', 16, 12, 0)
+        fbuf.text('VOLTA:%d'%volta, 4, 22, 0)
         return i2c.writeto(8,fbuf)
 
 def main():
